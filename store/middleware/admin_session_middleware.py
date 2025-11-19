82fe8d13f7cfb9abaee5a2fd5244461bf77f6be3
chrizz <christian.aribal@zdspgc.edu.ph>
2025-11-19 09:17:00 +0800

    delete

--- store/middleware/admin_session_middleware.py
+++
@@ -1,6 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class AdminSessionMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        if request.path.startswith("/admin"):
-            request.session_cookie_name = "admin_sessionid"
